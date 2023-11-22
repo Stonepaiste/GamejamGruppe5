5f172a45e139ea2571b635ae39337d84e91a43e2
JacobVigsoe <eh66xs@student.aau.dk>
2023-11-22 13:29:44 +0100

    noget

--- Assets/Scripts/PlayerMovement/MouseLook.cs
+++ Assets/Scripts/PlayerMovement/MouseLook.cs
@@ -22,7 +22,7 @@ public class MouseLook : MonoBehaviour
 
     void FixedUpdate() 
     {
-           float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
     
         xRotation -= mouseY;
