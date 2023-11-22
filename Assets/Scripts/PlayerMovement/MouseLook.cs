d1f21e59e54a2980ae6434527383ab6e68312d1e
jacob vigso <eh66xs@student.aau.dk>
2023-11-22 19:07:30 +0100

    fixed mouse sens

--- Assets/Scripts/PlayerMovement/MouseLook.cs
+++ Assets/Scripts/PlayerMovement/MouseLook.cs
@@ -16,16 +16,17 @@ public class MouseLook : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
         Cursor.lockState = CursorLockMode.Locked;
     }
+
 
     // Update is called once per frame
     void Update()
     {
-        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
-        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
-    
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.fixedDeltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.fixedDeltaTime;
+
         xRotation -= mouseY;
         xRotation = Mathf.Clamp(xRotation, -90f, 90f); //så man ikke kan roterer mere en 90 grader op og ned
 
